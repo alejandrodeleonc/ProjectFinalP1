4a5d6d4e1c6085bd882cba201e4e26376043aee9
Fernando Rojas <Fernando Rojas@LAPTOP-RDST2TR9>
2018-12-04 00:51:51 -0800

    Diseño de interfaz grafica clientes.

--- ProyectoFinalP1/src/Visual/CrearPlanes.java
+++ ProyectoFinalP1/src/Visual/CrearPlanes.java
@@ -34,35 +34,35 @@ public class CrearPlanes extends JDialog {
 	 * Create the dialog.
 	 */
 	public CrearPlanes() {
-		setBounds(100, 100, 398, 231);
+		setBounds(100, 100, 525, 290);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
 		textField = new JTextField();
-		textField.setBounds(133, 28, 230, 20);
+		textField.setBounds(122, 36, 323, 20);
 		contentPanel.add(textField);
 		textField.setColumns(10);
 		
 		JRadioButton rdbtnNewRadioButton = new JRadioButton("TV");
-		rdbtnNewRadioButton.setBounds(56, 100, 49, 23);
+		rdbtnNewRadioButton.setBounds(51, 143, 49, 23);
 		contentPanel.add(rdbtnNewRadioButton);
 		
 		JRadioButton rdbtnTelefono = new JRadioButton("Telefono");
-		rdbtnTelefono.setBounds(173, 100, 67, 23);
+		rdbtnTelefono.setBounds(207, 143, 74, 23);
 		contentPanel.add(rdbtnTelefono);
 		
 		JRadioButton rdbtnInternet = new JRadioButton("Internet");
-		rdbtnInternet.setBounds(295, 100, 67, 23);
+		rdbtnInternet.setBounds(371, 143, 74, 23);
 		contentPanel.add(rdbtnInternet);
 		
 		JLabel lblNombre = new JLabel("Nombre del plan");
-		lblNombre.setBounds(31, 31, 92, 14);
+		lblNombre.setBounds(25, 39, 92, 14);
 		contentPanel.add(lblNombre);
 		
 		JLabel lblCrearPlanes = new JLabel("Crear Planes");
-		lblCrearPlanes.setBounds(139, 11, 86, 14);
+		lblCrearPlanes.setBounds(207, 11, 86, 14);
 		contentPanel.add(lblCrearPlanes);
 		
 		JLabel label = new JLabel("");
@@ -70,16 +70,28 @@ public class CrearPlanes extends JDialog {
 		contentPanel.add(label);
 		
 		JSpinner spinner = new JSpinner();
-		spinner.setBounds(56, 73, 49, 20);
+		spinner.setBounds(51, 116, 49, 20);
 		contentPanel.add(spinner);
 		
 		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setBounds(173, 73, 67, 20);
+		spinner_1.setBounds(207, 116, 67, 20);
 		contentPanel.add(spinner_1);
 		
 		JSpinner spinner_2 = new JSpinner();
-		spinner_2.setBounds(295, 73, 68, 20);
+		spinner_2.setBounds(371, 116, 68, 20);
 		contentPanel.add(spinner_2);
+		
+		JLabel canales = new JLabel("Cantidad canales");
+		canales.setBounds(41, 92, 99, 14);
+		contentPanel.add(canales);
+		
+		JLabel minutos = new JLabel("Cantidad minutos");
+		minutos.setBounds(200, 91, 102, 14);
+		contentPanel.add(minutos);
+		
+		JLabel velocidad = new JLabel("Velocidad(mbps)");
+		velocidad.setBounds(357, 91, 100, 14);
+		contentPanel.add(velocidad);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
